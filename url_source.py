0d64ed16053b608a1dccb2effa9b5a1b4734e05c
Chris Kanich <ckanich@uic.edu>
2013-03-19 18:25:26 -0500

    test cases

--- url_source.py
+++ url_source.py
@@ -1,19 +1,20 @@
+import bz2
 import json
 from hashlib import md5
 import redis
 import sys
 import time
 
-'''add urls to the queue, one per second, from the alexa top 1M'''
+'''add urls to the queue, four per second, from the alexa top 1M'''
 
 infile = sys.argv[1]
 
 r = redis.StrictRedis(host='localhost',port=6379,db=0)
-with open(infile) as f:
+with bz2.BZ2File(infile) as f:
   for line in f.readlines():
     if len(line.strip()) > 0:
       url = 'http://' + line.split(',')[1].strip()
       insertme = json.dumps({'url':url})
       res = r.lpush('resque:crawlqueue',insertme)
-      # print "inserted",url
-      # time.sleep(.05)
+      print "inserted",url
+      time.sleep(.25)
