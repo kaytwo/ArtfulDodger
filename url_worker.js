d1b369725865df1a1fdc6e7f6ef0ef8a68aece34
Peter Snyder <snyderp@gmail.com>
2013-03-20 00:02:22 -0500

    First attempted rewrite of url worker

--- url_worker.js
+++ url_worker.js
@@ -2,93 +2,91 @@
 phantom.injectJs("resque.js");
 
 var heartbeat = 1,
-    lastheartbeat = 0;
-var system = require('system'),
-    fs = require('fs');
-/**
- * Render given urls
- * @param array of URLs to render
- * @param callbackPerUrl Function called after finishing each URL, including the last URL
- * @param callbackFinal Function called after finishing everything
- */
+    lastheartbeat = 0,
+    webpage = require('webpage'),
+    system = require('system'),
+    fs = require('fs'),
+    out_queue_name = "resultqueue",
+    in_queue_name  = "crawlqueue",
+    queue,
+    process_result = function (a_status, a_url, a_dom) {
 
-function RenderUrlsToFile(callbackPerUrl, callbackFinal) {
-	var urlIndex = 0, // only for easy file naming
-    	webpage = require('webpage'),
-		page = webpage.create();
-	var getFilename = function() { return 'rendermulti-' + urlIndex + '.png'; }
-	var next = function(status, url, file) {
-		// page.close();
-		callbackPerUrl(status, url, file);
-		retrieve();
-	}
-	var retrieve = function() {
-    heartbeat++;
-    queue.pop(inq,function(url){
-      if (url){
-        if ('url' in url) {
-          urlIndex++;
-          // page = webpage.create();
-          page.viewportSize = { width: 800, height : 600 };
-          page.settings.loadImages = false;
-          page.settings.userAgent = "Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US) AppleWebKit/532+ (KHTML, like Gecko) Version/4.0.2 Safari/530.19.1";
-          page.onConsoleMessage = function(msg) {};
-          page.onError = function(msg,trace){};
-          page.open(url['url'], function(status) {
-            // page.evaluate(function() { document.body.bgColor = 'white';});
-            // var file = getFilename();
-            // todo: maybe take a breather and reboot the webpage if urlIndex % 100 = 99
-            window.setTimeout(function() {
-              renderedDom = page.content;
-              // page.render(file);
-              next(status, url['url'], renderedDom);
-             }, 25);
-            
-          });
-        }
-        else{
-          console.log("discarding garbage from queue.");
-          // retrieve();
-        }
-      }
-      else {
-          console.log("got nothing, finishing");
-          page.close();
-          callbackFinal();
-      }
-    });
-	}
-  retrieve();
-}
+        var now = new Date().getTime();
+        heartbeat++;
 
-function work(status, url, dom){
-    now = new Date().getTime();
-    queue.push(outq,{"url":url,"dom":dom,"visitts":now});
-		console.log("Rendered '" + url + "' at '" + now + "'");
-}
+        queue.push(out_queue_name, {
+            url: a_url,
+            dom: a_dom,
+            visits: now
+        });
 
-function waitrepeat(){
-  heartbeat++;
-  console.log("exhausted queue. sleeping for it to refill");
-  setTimeout(function(){RenderUrlsToFile(work,waitrepeat);},5000);
-}
+        console.log("Rendered '" + a_url + "' at '" + now + "'");
+    },
+    read_queue = function () {
 
-function startwork(resque) {
-  RenderUrlsToFile(work, waitrepeat);
-}
-var outq = "resultqueue",
-    inq  = "crawlqueue";
+        queue.pop(in_queue_name, function (item) {
 
-var queue = new Resque("localhost","7379",startwork);
+            if (!item) {
 
-setInterval(function(){
-  // page hasn't successfully loaded in N seconds, die and be reborn
-  if(lastheartbeat == heartbeat){
-    phantom.exit();
-  }
-  lastheartbeat = heartbeat;
-},15000);
+                queue_empty();
 
-// var queue = new Resque("localhost","7379",function(){console.log("started");});
-// setTimeout(function(){RenderUrlsToFile(work, waitrepeat)},500);
-// RenderUrlsToFile(arrayOfUrls, work , waitrepeat);
+            } else if (!item.url) {
+
+                console.log("discarding garbage from queue.");
+
+            } else {
+
+                a_page.open(item.url, function (status) {
+
+                    console.log("just finished reading " + item.url);
+                    window.setTimeout(function () {
+
+                        // Do something with rendered dom, why not?
+                        process_result(status, item.url, a_page.content);
+                        read_queue();
+
+                    }, 25);
+                });
+            }
+        });
+    },
+    create_page = function () {
+
+        var page = webpage.create();
+
+        page.viewportSize = {
+            width: 800,
+            height : 600
+        };
+        page.settings.loadImages = false;
+        page.settings.userAgent = "Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US) AppleWebKit/532+ (KHTML, like Gecko) Version/4.0.2 Safari/530.19.1";
+        page.onConsoleMessage = function(msg) {};
+        page.onError = function(msg, trace) {};
+
+        return page;
+    },
+    // For now we can try just creating a single page object and reusing it,
+    // but might need to fall back to creating one per request
+    a_page = create_page(),
+    queue_empty = function () {
+
+        heartbeat++;
+        console.log("exhausted queue. sleeping for it to refill");
+        setTimeout(function () {
+            read_queue();
+        }, 5000);
+    };
+
+queue = new Resque("localhost", "7379", function (resque) {
+    read_queue();
+});
+
+setInterval(function () {
+
+    // page hasn't successfully loaded in N seconds, die and be reborn
+    if (lastheartbeat === heartbeat) {
+        phantom.exit();
+    }
+    lastheartbeat = heartbeat;
+
+}, 15000);
