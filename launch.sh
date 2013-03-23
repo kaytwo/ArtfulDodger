c6ee2172b36cb2da974b36b0eec6e2ebe90fde84
Chris Kanich <ckanich@uic.edu>
2013-03-23 18:01:35 -0500

    tweaks to startup

--- launch.sh
+++ launch.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-for i in `seq 1 25` ; do 
-  (while true ; do phantomjs url_worker.js 2>> /tmp/phantom_errors ; done) &
+# launch 25 instances by default; first arg can be diff number of instances
+for i in `seq 1 ${1:-25}` ; do 
+  (while true ; do phantomjs url_worker.js > /dev/null 2>> /tmp/phantom_errors ; done) &
 done
