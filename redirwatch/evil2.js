d39d761b7c0790924aa719eec94375316aa03bf2
Chris Kanich <ckanich@uic.edu>
2013-03-25 17:40:27 +0000

    testhook tweaks

--- redirwatch/evil2.js
+++ redirwatch/evil2.js
@@ -3,5 +3,7 @@ window.currentScript = window.currentScript[window.currentScript.length-1].src;
 console.log("double nothing happened.");
 document.getElementById('target').innerHTML = '<span>bangarang rufio!</span>';
 // '<iframe src="test2.html></iframe>';
-document.getElementById('derp2').src="evil3.html";
-console.log("iframe created?");
+setTimeout(function(){
+  document.getElementById('derp2').src="evil3.html";
+  console.log("iframe src modified by evil2.js");
+},500);
