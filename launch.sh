ba4d8abbc096edf33b674cb5e7e0e158ff1cc606
Chris Kanich <ckanich@uic.edu>
2013-03-27 18:51:13 -0500

    slightly more portable launch script

--- launch.sh
+++ launch.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 # launch 25 instances by default; first arg can be diff number of instances
+# ex: ./launch.sh NUMPROCS SCRIPT_PATH
+SCRIPT_PATH=${2:-"/home/ckanich/workspace/ArtfulDodger/"}
+cd $SCRIPT_PATH
 for i in `seq 1 ${1:-25}` ; do 
   (while true ; do phantomjs url_worker.js > /dev/null 2>> /tmp/phantom_errors ; done) &
 done
