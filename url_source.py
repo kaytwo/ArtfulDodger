c9b3403cb237853c9d50594526eba3ac427f0f69
domcrawler <domcrawler@Merohely-1.crysys.hu>
2013-04-17 00:18:23 -0500

    source split into testcase from alexa vs. from specific file

--- url_source.py
+++ url_source.py
@@ -4,23 +4,26 @@ from hashlib import md5
 import redis
 import sys
 import time
+import urlparse
 
-'''add urls to the queue, four per second, from the alexa top 1M'''
+'''add urls to the queue from a file of newline separated URLs'''
 
-infile = 'top-1m.csv.bz2'
-numurls = int(sys.argv[1]) if len(sys.argv) > 1 else 100
-r = redis.StrictRedis(host='localhost',port=6379,db=0)
+if len(sys.argv) != 2:
+  print "first arg is list of newline-separated URLs to crawl"
+  sys.exit()
+infile = sys.argv[1]
+r = redis.StrictRedis(host='10.105.1.41',port=6379,db=0)
 urls_added = 0
 try:
-  f = bz2.BZ2File(infile)
+  f = open(infile)
   for line in f.readlines():
+    
     if len(line.strip()) > 0:
-      url = 'http://' + line.split(',')[1].strip()
-      insertme = json.dumps({'url':url})
+      url = line.strip()
+      insertme = json.dumps({'tag':infile,'url':url})
       res = r.lpush('resque:crawlqueue',insertme)
       urls_added += 1
-      if numurls > 0 and urls_added == numurls:
-        print "inserted %d urls" % urls_added
-        sys.exit()
 except:
+  print "error with the file"
   pass
+print "inserted %d urls" % urls_added
